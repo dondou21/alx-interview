0faf7bbc80f883c74bbdd674338c639e095200b2
Dondou <106757087+dondou21@users.noreply.github.com>
2024-09-17 12:36:25 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-
 """
-Making change
+Coin Change Algorithm
 """
 
 
 def makeChange(coins, total):
+    """Calculate the fewest number needed to meet,
+    needed to meet a given total amount.
+    Args:
+        coins ([list]): A list of coin values available.
+        total ([number]): The target amount
+    Return: The fewest number of coins needed to reach the total,
+    or -1 if not possible.
+    """
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
